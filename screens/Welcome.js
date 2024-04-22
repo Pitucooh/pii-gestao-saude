5abdb36d20c98b0c04642ee85cd5d8cf01bce212
vewadie <verinawadie1@gmail.com>
2024-04-22 20:08:02 -0300

    frontend temporario login/cadastro

---
+++ screens/Welcome.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+
+import{
+    InnerContainer,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    StyledButton,
+    ButtonText,
+    Line,
+    WelcomeContainer,
+    WelcomeImage,
+    Avatar
+} from './../components/styles';
+
+
+const Welcome = ({navigation}) => {
+    return (
+        <>
+            <StatusBar style = "light" />
+            <InnerContainer>
+                <WelcomeImage resizeMode= "cover" source = {require('./../assets/logo.png')}/>
+                <WelcomeContainer>
+                    <PageTitle welcome={true}>Bem vindo!</PageTitle>
+                    <SubTitle welcome={true}>Fulano</SubTitle>
+                    <StyledFormArea>
+                    <Avatar resizeMode= "cover" source = {require('./../assets/logo.png')} />
+                    <Line />
+                        <StyledButton onPress={() => {navigation.navigate('Login')}}>
+                            <ButtonText>Logout</ButtonText>
+                        </StyledButton>      
+                    </StyledFormArea>
+                </WelcomeContainer>
+            </InnerContainer>
+        </>
+    );
+};
+
+
+export default Welcome;
