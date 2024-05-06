3bc041d15240c55148e40cf623972846168f247f
febeatriz <febiatx@gmail.com>
2024-05-06 16:05:53 -0300

    pressao arrumada

--- pressaoArterial.js
+++ pressaoArterial.js
@@ -5,29 +5,30 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function pressaoArterial (sistolica, diastolica){
-    if(sistolica < 90 && diastolica < 60){
+function pressaoArterial(sistolica, diastolica) {
+    if (sistolica < 90 && diastolica < 60) {
         return "Pressão baixa";
-    } else if(90<= sistolica < 120 && 60<=diastolica < 80){
+    } else if (90 <= sistolica && sistolica < 120 && 60 <= diastolica && diastolica < 80) {
         return "Pressão ótima";
-    } else if (120 <= sistolica <= 129 && 80 <= diastolica <= 84){
+    } else if (120 <= sistolica && sistolica <= 129 && 80 <= diastolica && diastolica <= 84) {
         return "Pressão normal";
-    } else if(130<= sistolica <= 139 && 85 <= diastolica <= 89){
+    } else if (130 <= sistolica && sistolica <= 139 && 85 <= diastolica && diastolica <= 89) {
         return "Atenção";
-    } else{
+    } else if (sistolica>=140 && diastolica >=90) {
         return "Pressão alta";
     }
 }
 
-function infos(){
-    rl.question("Informe sua pressão sistólica: ", function(sistolica){
-        rl.question("Informe sua pressão diastlica: ", function(diastolica){
+function infos() {
+    rl.question("Informe sua pressão sistólica: ", function (sistolica) {
+        rl.question("Informe sua pressão diastólica: ", function (diastolica) {
             sistolica = parseInt(sistolica);
             diastolica = parseInt(diastolica);
-            var result = pressaoArterial (sistolica);
+            var result = pressaoArterial(sistolica, diastolica); // Corrigido para passar ambos os argumentos
             console.log(result);
             rl.close();
         });
     });
 }
+
 infos();
