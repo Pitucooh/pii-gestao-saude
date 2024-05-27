51b0ad94e0f7a8c820c875d69dbd0eaea986222f
João Cruz <132522413+Pitucooh@users.noreply.github.com>
2024-05-26 22:23:59 -0300

    ighnorem

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Mamacosma2!",
+  password: "suasenha",
   database: "wepink",   
   port: 3306,
 });
