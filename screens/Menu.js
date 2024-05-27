ee696d507ebd9fe62e744fd9ff0f1b57f4cfd8ed
vewadie <verinawadie1@gmail.com>
2024-05-27 09:10:40 -0300

    teclado menu

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,9 +1,7 @@
-import React, {useState, useContext} from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-
-
 
 import KeyboardWrapper from '../components/KeyboardWrapper';
 import {
@@ -12,115 +10,91 @@ import {
     PageTitle,
     SubTitle,
     StyledFormArea,
-    LeftIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    RightIcon,
     StyledButton,
-    ButtonText,
     Colors,
-    MsgBox,
-    Line,
-    ExtraText,
-    ExtraView,
-    TextLink,
-    TextLinkContent, 
     WelcomeContainer
 } from './../components/styles';
 
-const {primary, secondary, tertiary, darkLight, brand, green, red, customGreen, backgroundGreen, green2, greenForm, black, roxinho} = Colors;
+const { primary, secondary, tertiary, darkLight, brand, green, red, customGreen, backgroundGreen, green2, greenForm, black, roxinho } = Colors;
 
-const Menu = ({navigation}) => {  
-    
+const Menu = ({ navigation }) => {
+
     const handleCardPress = (screenName) => {
         navigation.navigate(screenName);
     };
 
-
-    
     return (
         <>
-            <StatusBar style = "light" />
-            <InnerContainer>
-                <WelcomeContainer>
-                   
-                    <PageTitle welcome={true} style={{flexWrap: 'wrap', lineHeight: 30, color:customGreen, 
-                         fontSize: 30, marginTop: 25}}>MENU</PageTitle>
-                    <View style={{ height: 2, backgroundColor: roxinho, width: '50%' }}></View>
-                    <SubTitle welcome={true} style={{ color: greenForm, marginTop: 10 }}>Seja bem-vindo!</SubTitle>
-                    <Text style={{color: roxinho, marginBottom:20}}>{'Monitore a sua saúde'} </Text>
-                    
-                    <View style={styles.notesContainer}>
-                        <Text style={styles.notesText}>Bloco de Notas:</Text>
-                        <TextInput
-                            style={styles.notesInput}
-                            multiline={true}
-                            numberOfLines={4}
-                            placeholder="Digite suas notas aqui..."
-                        />
-                    </View>
-                    <View style={styles.container}>
-                    <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Exames')}>
-                        <Text style={styles.cardText}>Exames</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Consultas')}>
-                        <Text style={styles.cardText}>Consultas</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Medicacoes')}>
-                        <Text style={styles.cardText}>Medicações</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Meusdados')}>
-                        <Text style={styles.cardText}>Meus Dados</Text>
-                    </TouchableOpacity>
-                    </View>
+            <StatusBar style="light" />
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <InnerContainer>
+                    <WelcomeContainer>
+                        <PageTitle welcome={true} style={{ flexWrap: 'wrap', lineHeight: 30, color: customGreen, fontSize: 30, marginTop: 25 }}>MENU</PageTitle>
+                        <View style={{ height: 2, backgroundColor: roxinho, width: '50%' }}></View>
+                        <SubTitle welcome={true} style={{ color: greenForm, marginTop: 10 }}>Seja bem-vindo!</SubTitle>
+                        <Text style={{ color: roxinho, marginBottom: 20 }}>{'Monitore a sua saúde'} </Text>
 
+                        <View style={styles.notesContainer}>
+                            <Text style={styles.notesText}>Bloco de Notas:</Text>
+                            <TextInput
+                                style={styles.notesInput}
+                                multiline={true}
+                                numberOfLines={4}
+                                placeholder="Digite suas notas aqui..."
+                            />
+                        </View>
+                        <View style={styles.container}>
+                            <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Exames')}>
+                                <Text style={styles.cardText}>Exames</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Consultas')}>
+                                <Text style={styles.cardText}>Consultas</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Medicacoes')}>
+                                <Text style={styles.cardText}>Medicações</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.card} onPress={() => handleCardPress('Meusdados')}>
+                                <Text style={styles.cardText}>Meus Dados</Text>
+                            </TouchableOpacity>
+                        </View>
 
-                    <StyledFormArea>
-                    <View style={{ height: 2, backgroundColor: roxinho, width: '100%', marginTop:15, marginBottom: 15}}></View>
-
-                          
-                    </StyledFormArea>
-                </WelcomeContainer>
-            </InnerContainer>
-            
+                        <StyledFormArea>
+                            <View style={{ height: 2, backgroundColor: roxinho, width: '100%', marginTop: 15, marginBottom: 15 }}></View>
+                        </StyledFormArea>
+                    </WelcomeContainer>
+                </InnerContainer>
+            </TouchableWithoutFeedback>
         </>
     );
 };
 
-
 const styles = StyleSheet.create({
-
-    container:{
-        backgroundColor: greenForm, 
+    container: {
+        backgroundColor: greenForm,
         width: 300,
         borderRadius: 20,
         height: 350,
         alignItems: 'flex-start'
-    }, 
-    
+    },
     card: {
-        backgroundColor: backgroundGreen, 
+        backgroundColor: backgroundGreen,
         width: 110,
-        marginRight:1000,
+        marginRight: 1000,
         flex: 1,
         marginHorizontal: 5,
         justifyContent: 'center',
         alignItems: 'center',
-        
     },
     cardText: {
-      fontSize: 18,
-      color: greenForm
-      
+        fontSize: 18,
+        color: greenForm
     },
-
-    footer:{
+    footer: {
         backgroundColor: greenForm,
         width: 500,
-        marginTop:200,
+        marginTop: 200,
         flex: 1
     },
-
     notesContainer: {
         backgroundColor: roxinho,
         borderRadius: 10,
@@ -143,10 +117,6 @@ const styles = StyleSheet.create({
         padding: 5,
         color: customGreen
     },
-
-
-  });
-
-
+});
 
 export default Menu;
