6e6401e8c2f7f5b3736c0d7e47fbe218cf19c998
bleer <125562229+bleer2004@users.noreply.github.com>
2024-05-27 00:39:24 -0300

    Update Meusdados.js

--- screens/Meusdados.js
+++ screens/Meusdados.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
+const { brand, darkLight, backgroundGreen, customGreen, primary, greenForm, roxinho } = Colors;
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    StyledButton,
+    ButtonText,
+    Colors,
+    MsgBox,
+    Line,
+    ExtraText,
+    ExtraView,
+    TextLink,
+    TextLinkContent,
+    WelcomeContainer,
+} from './../components/styles';
+
 const Meusdados = ({ navigation }) => {
 
     const handleIMCButtonPress = () => {
@@ -16,44 +40,112 @@ const Meusdados = ({ navigation }) => {
     };
 
     return (
-        <View style={styles.container}>
-              <Text style={styles.profileText}>Nome: João Silva</Text>
-                <Text style={styles.profileText}>Local: São Paulo, SP</Text>
-                <Text style={styles.profileText}>Idade: 30 anos</Text>
-                <Text style={styles.profileText}>Email: joao.silva@example.com</Text>
-                <Text style={styles.profileText}>Telefone: (11) 98765-4321</Text>
-            <TouchableOpacity style={styles.metricButton} onPress={handleIMCButtonPress}>
-                <Text style={styles.buttonText}>IMC</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.metricButton} onPress={handlePressaoButtonPress}>
-                <Text style={styles.buttonText}>Pressão Arterial</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.metricButton} onPress={handleGlicemiaButtonPress}>
-                <Text style={styles.buttonText}>Glicemia</Text>
-            </TouchableOpacity>
-        </View>
-    );
-};
+        <InnerContainer style={{backgroundColor: backgroundGreen}}>
+        <WelcomeContainer>
+                    
+                    <PageTitle welcome={true} style={{flexWrap: 'wrap', lineHeight: 30, color:customGreen, 
+                         fontSize: 30, marginTop: 30}}>MEUS DADOS</PageTitle>
+                    <Text style={{color: roxinho, alignItems: 'center'}}>{'Salve aqui os seus dados para maior facilidade'} </Text>
+                    
+                    <View style={styles.container}>
+                    <View style={styles.profileContainer}>
+                        <Text style={styles.profileTitle}>Perfil</Text>
+                        <Text style={styles.profileText}>Nome: Fulno da Silva</Text>
+                        <Text style={styles.profileText}>Idade: 30 anos</Text>
+                        <Text style={styles.profileText}>Email: fulano.silva@gmail.com</Text>
+                    </View>
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+                    
+                    <View style={styles.metricsGrid}>
+                        <View style={styles.metricItem}>
+                        <TouchableOpacity style={styles.metricButton} onPress={handleIMCButtonPress}>
+                            <Text style={styles.buttonText}>IMC</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.metricInfo}>22.5</Text>
+                        </View>
+                        
+                        <View style={styles.metricItem}>
+                        <TouchableOpacity style={styles.metricButton} onPress={handlePressaoButtonPress}>
+                            <Text style={styles.buttonText}>Pressão Arterial</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.metricInfo}>120/80</Text>
+                        </View>
+                        
+                        <View style={styles.metricItem}>
+                        <TouchableOpacity style={styles.metricButton} onPress={handleGlicemiaButtonPress}>
+                            <Text style={styles.buttonText}>Glicemia</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.metricInfo}>90 mg/dL</Text>
+                        </View>
+                        </View>
+                    </View>
+                </WelcomeContainer>
+     </InnerContainer>
+        
+      );
+    };
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 0.9,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f8f9fa',
-    },
-    metricButton: {
-        marginVertical: 10,
-        width: '80%',
-        paddingVertical: 15,
-        backgroundColor: '#007bff',
-        borderRadius: 5,
+        backgroundColor: backgroundGreen,
+      },
+      profileContainer: {
+        marginBottom: 30,
+        padding: 20,
+        width: '100%',
+        backgroundColor: greenForm,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+        
+      },
+      profileTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
+        color: backgroundGreen
+      },
+      profileText: {
+        fontSize: 18,
+        color: backgroundGreen,
+        marginBottom: 5,
+      },
+      metricsGrid: {
+        width: '90%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      },
+      metricItem: {
         alignItems: 'center',
-    },
-    buttonText: {
+        marginVertical: 10,
+      },
+      metricButton: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:roxinho,
+        borderRadius: 10,
+        
+      },
+      metricInfo: {
+        marginTop: 10,
+        fontSize: 16,
+        color: '#333',
+        borderWidth: 2, 
+        padding: 20
+      },
+      buttonText: {
         color: '#fff',
         fontSize: 16,
-    },
-});
-
+        textAlign: 'center',
+      },
+    });
 export default Meusdados;
